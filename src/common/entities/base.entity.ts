6634e35f3780ad633e7ed833f81adc2e652885fe
Natpon <25012514amonwan@gmail.com>
2026-03-03 14:33:50 +0700

    fix abstract

--- src/common/entities/base.entity.ts
+++ src/common/entities/base.entity.ts
@@ -1,16 +1,15 @@
-import { PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm' 
-
 export abstract class BaseEntity {
+  readonly id: string
+  readonly createdAt: Date
+  updatedAt: Date
 
+  constructor(id: string) {
+    this.id = id
+    this.createdAt = new Date()
+    this.updatedAt = new Date()
+  }
 
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
-
-    @CreateDateColumn()
-    createdAt: Date;
-
-    @UpdateDateColumn()
-    updatedAt: Date;
-    //test
-
+  markUpdated(): void {
+    this.updatedAt = new Date()
+  }
 }
