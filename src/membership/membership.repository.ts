adb67a02936c393327397b3260d7de630c94d5ff
Natpon <25012514amonwan@gmail.com>
2026-03-08 13:05:10 +0700

    fix get member by id organization

--- src/membership/membership.repository.ts
+++ src/membership/membership.repository.ts
@@ -4,7 +4,7 @@ import { promises as fs } from 'fs';
 import * as path from 'path';
 import { Membership } from './entities/membership.entity';
 
-const DB_PATH = path.join(process.cwd(), 'database', 'membership.json');
+const DB_PATH = path.join(process.cwd(), 'src', 'database', 'memberships.json');
 
 @Injectable()
 export class MembershipRepository {
