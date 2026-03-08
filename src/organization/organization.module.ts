b44f036066b3e11c60e59cd71a09623c6dd3d74a
Natpon <25012514amonwan@gmail.com>
2026-03-08 13:46:42 +0700

    get/org/id/member is easy to read now

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,16 +1,20 @@
+// src/organization/organization.module.ts
 import { Module } from '@nestjs/common';
 import { OrganizationService } from './organization.service';
 import { OrganizationController } from './organization.controller';
-import { OrganizationRepository } from './organization.repository'; // <-- 1. Import it
+import { OrganizationRepository } from './organization.repository'; 
 
 import { MembershipModule } from '@/membership/membership.module';
+// 🌟 1. เพิ่ม Import UserModule เข้ามาตรงนี้
+import { UserModule } from '@/user/user.module'; 
 
 @Module({
-  imports: [MembershipModule],
+  // 🌟 2. เอา UserModule มาใส่ใน array นี้ด้วยครับ
+  imports: [MembershipModule, UserModule], 
   controllers: [OrganizationController],
   providers: [
     OrganizationService, 
-    OrganizationRepository // <-- 2. Add it to providers
+    OrganizationRepository 
   ],
   exports: [OrganizationService]
 })
